e1f83a0c84e9a2bea7a8ed640f6f5e8841c9435f
Phong Tran <tran.q.phong@gmail.com>
2015-09-11 22:20:03 -0500

    New stuff from ch18 and exercise 17

--- 18.3_FactorialCalculator/src/FactorialCalculator.java
+++ 18.3_FactorialCalculator/src/FactorialCalculator.java
@@ -5,11 +5,11 @@
 public class FactorialCalculator
 {
     //  recursive method factorial (assumes its parameter is >= 0)
-    public long factorial (long number)
+    public static long factorial(long number)
     {
-        if (number <= 1)
-            return 1;   //  test for base case
-        else
+        if (number <= 1)    //  test for base case
+            return 1;   //  base cases: 0! = 1 and 1! = 1
+        else    //  recursion step
             return number * factorial(number - 1);
     }   //  end method factorial
 
